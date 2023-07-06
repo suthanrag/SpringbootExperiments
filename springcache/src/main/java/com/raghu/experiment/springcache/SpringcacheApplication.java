d3b5ae959240532814f12664884ee9a3cd6e725a
suthanrag <raghu.karthic@gmail.com>
2023-07-06 11:58:12 -0400

    Initial Commit for the Cache Project

---
+++ springcache/src/main/java/com/raghu/experiment/springcache/SpringcacheApplication.java
@@ -0,0 +1,14 @@
+package com.raghu.experiment.springcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+public class SpringcacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcacheApplication.class, args);
+	}
+
+}
---
+++ springcache/src/test/java/com/raghu/experiment/springcache/SpringcacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raghu.experiment.springcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
